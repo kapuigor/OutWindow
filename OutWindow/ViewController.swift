9c66e97e453783cb3dc0d46d0e3f737e3301196e
Igor Kapustin <kapui-igor@ya.ru>
2021-10-02 17:28:47 +0300

    Initial Commit

---
+++ OutWindow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutWindow
+//
+//  Created by Игорь Капустин on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
